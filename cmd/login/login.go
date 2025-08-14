259a1c26c85fc4e720bc1c0dd4d9525e1233114b
sewn <sewn@disroot.org>
2025-08-14 13:14:55 +0300

    feat(cmd/login): add debugTransport

--- cmd/login/login.go
+++ cmd/login/login.go
@@ -1,19 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"log/slog"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/sewnie/rbxweb"
 )
 
-func main() {
-	slog.SetLogLoggerLevel(slog.LevelDebug)
+type debugTransport struct {
+	underlying http.RoundTripper
+}
 
+func (t *debugTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	slog.Debug("HTTP request",
+		"method", req.Method,
+		"url", req.URL.String(),
+	)
+
+	if req.Body != nil {
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+		req.Body = io.NopCloser(bytes.NewReader(body))
+		slog.Debug("HTTP request body", "body", string(body))
+	}
+
+	resp, err := t.underlying.RoundTrip(req)
+	if err != nil {
+		slog.Debug("HTTP request failed",
+			"error", err,
+		)
+		return nil, err
+	}
+
+	slog.Debug("HTTP response",
+		"status", resp.StatusCode,
+	)
+	return resp, nil
+}
+
+func main() {
 	c := rbxweb.NewClient()
-	c.Logger = slog.Default()
+	slog.SetLogLoggerLevel(slog.LevelDebug)
+	c.Client.Transport = &debugTransport{
+		underlying: http.DefaultTransport,
+	}
 
 	if len(os.Args) == 3 {
 		log.Fatal(c.AuthV2.CreateLogin(os.Args[1], os.Args[2], rbxweb.LoginTypeUsername))
@@ -29,6 +66,7 @@ func main() {
 		if err != nil {
 			log.Fatalln("token status:", err)
 		}
+		log.Println(s)
 
 		if s.Status == "Validated" {
 			break
